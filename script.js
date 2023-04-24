1ec1f876769fcacaa1539628e39170b642500262
Evran0 <eddypolisabuzi59@gmail.com>
2023-04-24 19:55:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+1. Differences entre "var"et "let";
+
+=> var = est utilisé pour déclarer une variable globale(sans distinction des blocs utilises dans la fonction). tandis que
+    => let = est utilisé pour déclarer une variable dont la portée est limitée à un bloc.
+
+2.
+var x = 1 / 6;
+var y = 3 / 2;
+var z = x * y;
+var b = 4 / 3;
+var c = 1 / 2;
+var d = b + c;
+var = z/d;
